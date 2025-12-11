4e75a3572eaf14df0badde3a0e54da71f8c8fcdd
user <yutong.wu@mailbox.tu-dresden.de>
2025-12-11 20:53:40 +0100

    ADD RegisterPage route and link in App.jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage";
 import { BrowserRouter, Routes, Route, Link} from "react-router-dom"
 import "./App.css"
@@ -9,11 +10,13 @@ return (
   <BrowserRouter>
     <nav>
     <Link to="/">Home</Link>
+    <Link to="/register">Register</Link>
     <Link to="/login">Login</Link>
     </nav>
     <Routes>
       <Route path="/"element= {<HomePage/>} />
-      <Route path="/login"element= {<LoginPage/>} />    
+      <Route path="/register"element= {<RegisterPage/>} />   
+      <Route path="/login"element= {<LoginPage/>} />  
     </Routes>
   </BrowserRouter>
 )
