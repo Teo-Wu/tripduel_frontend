aafa7197c7c7691d88934210f6aef2eb3517f97c
user <yutong.wu@mailbox.tu-dresden.de>
2025-12-11 20:55:07 +0100

    EDIT LoginPage.jsx to combine with backend user

--- my-react-app/src/pages/LoginPage.jsx
+++ my-react-app/src/pages/LoginPage.jsx
@@ -1,22 +1,45 @@
-import { useState } from 'react'
+import { useState } from "react"
+import { useNavigate } from "react-router-dom";
 
 function LoginPage() {
   const [username, setUsername] = useState("");
-  const [password,setPassword] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError]=useState("");
-  
-  const handleLogin = () => {
+  const [success, setSuccess] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
     if (!username || !password){
       setError("please fill in both fields");
+      setSuccess("");
+      return; 
     }
-    if (username === "dm1" && password === "pwd1") {
-      setError("");
-      alert("login Successful!");
-    }
-    else {
-      setError("Invalid username or password.")
-    }
-  }
+
+    try {
+      const response = await fetch("http://localhost:5000/api/auth/login",{
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username,password })
+      });
+      const data = await response.json();
+
+      if (response.ok) {
+        setError("");
+        setSuccess("Logged in successfully!");
+        // Optionally redirect to another page
+        setTimeout(() => navigate("/"), 1000);
+
+      } else {
+        setError(data.error || data.msg || "Registration failed");
+        setSuccess("");
+      }
+    } catch (err) {
+      console.error(err);
+      setError("Server error. Try again later.");
+      setSuccess("");
+    }  
+  };
+
   return (
     <>
       <div> 
@@ -29,7 +52,8 @@ function LoginPage() {
         <input type = "password" placeholder = "Password" 
           value = {password} onChange = {(e) => setPassword(e.target.value)}>
           </input>
-        { error && <p> {error} </p> }
+        {error && <p style={{ color: "red" }}> {error} </p> }
+        {success && <p style={{ color: "green" }}> {success} </p> }
         <button onClick = {handleLogin}> Login </button>
       </div>
     </>
